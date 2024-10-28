2f526bb94fd235fd9bb6f0a0ddaa181f6a0e827d
zachkaupp <zachkaupp@gmail.com>
2024-10-28 09:30:25 -0500

    update package __main__

--- census_transportation/__main__.py
+++ census_transportation/__main__.py
@@ -3,9 +3,25 @@ main.py
 zachkaupp@gmail.com
 """
 
+from .data_process.acs import main as process_acs
+from .data_process.travel_time import main as process_travel_time
+
 def main():
     """main()"""
-    print("hello world")
+    print("Cleaning data files...")
+    try:
+        process_acs()
+        print("Cleaned acs_data.csv")
+    except Exception as e:
+        print("Failed to clean acs_data.csv")
+        raise e
+
+    try:
+        process_travel_time()
+        print("Cleaned travel_time_to_work.csv")
+    except Exception as e:
+        print("Failed to clean travel_time_to_work.csv")
+        raise e
 
 if __name__ == "__main__":
     main()
